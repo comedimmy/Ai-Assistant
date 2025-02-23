49fac9acb77d68ed1c10398465e5742ccc700f7e
comedimmy <113405460+comedimmy@users.noreply.github.com>
2025-02-24 02:37:59 +0800

    Update 測試用flask伺服器.py

--- 測試用flask伺服器.py
+++ 測試用flask伺服器.py
@@ -10,15 +10,42 @@ import openai
 import paho.mqtt.client as mqtt
 from flask import Flask, redirect, url_for, session,render_template
 from authlib.integrations.flask_client import OAuth
-from app import auth_bp  # 匯入處理 OAuth 的藍圖
+from datetime import timedelta
+from auth_decorator import login_required
 
+# dotenv setup
+from dotenv import load_dotenv
+
+load_dotenv()
 # 設定路徑
 os.chdir("f:/water")
+
+# App config
+app = Flask(__name__)
+# Session config
+app.secret_key = os.getenv("APP_SECRET_KEY")
+app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
+# oAuth Setup
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    client_kwargs={'scope': 'email profile'},
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration'
+)
+
 
 
 app = Flask(__name__)
 app.secret_key = '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'
-app.register_blueprint(auth_bp, url_prefix='/auth')
 
 openai.api_key = "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"
 
@@ -174,9 +201,15 @@ def receive_data():
 
 
 
+
+@login_required
 @app.route("/")
 def index():
     return render_template("index.html")  # index.html
+
+def hello_world():
+    email = dict(session)['profile']['email']
+    return f'Hello, you are logge in as {email}!'
 
 @app.route("/login-page")
 def login_page():
@@ -184,8 +217,34 @@ def login_page():
 
 @app.route("/back")
 def back():
-    return render_template("index.html")  # login.html
+    return render_template("index.html")  # index.html
 
+@app.route('/login')
+def login():
+    google = oauth.create_client('google')  # create the google oauth client
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+
+@app.route('/authorize')
+def authorize():
+    google = oauth.create_client('google')  # create the google oauth client
+    token = google.authorize_access_token()  # Access token from google (needed to get user info)
+    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
+    user_info = resp.json()
+    user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
+    # Here you use the profile/user data that you got and query your database find/register the user
+    # and set ur own data in the session not the profile from google
+    session['profile'] = user_info
+    session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
