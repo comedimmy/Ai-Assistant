b52723758d6ad8ec3ab61d91b16b6562df5af128
comedimmy <113405460+comedimmy@users.noreply.github.com>
2025-05-11 22:39:31 +0800

    Delete new water directory

--- new water/api/web_change.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint,render_template,redirect,request
-
-web_bp = Blueprint('web',__name__)
-
-@web_bp.route('/test')
-def test():
-    return render_template('test.html')  #test.html
